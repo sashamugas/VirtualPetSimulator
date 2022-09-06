447ca91dbc1cf64f94611f218fc6b8e9ef2c88f2
sashamugs <richard.sasha116@gmail.com>
2022-09-06 15:13:11 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace VirtualPetSimulator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the pet simulator");
+            Console.ReadKey();
+        }
+    }
+}

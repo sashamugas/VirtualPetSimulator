6da20b284bd6275334a8f7f5eeb208adb780c5f0
sashamugs <richard.sasha116@gmail.com>
2022-09-09 10:29:45 +0300

    OOP

--- Program.cs
+++ Program.cs
@@ -1,11 +1,50 @@
-﻿namespace VirtualPetSimulator
+﻿using static System.Console;
+
+namespace VirtualPetSimulator
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the pet simulator");
-            Console.ReadKey();
+            //title on console window
+            Title ="Virtual Pet Simulator";
+                       
+
+            WriteLine(@"
+ _     _  ____  _____  _     ____  _       ____  __________    ____  _  _     _     _    ____  _____  ____  ____ 
+/ \ |\/ \/  __\/__ __\/ \ /\/  _ \/ \     /  __\/  __/__ __\  / ___\/ \/ \__// \ /\/ \  /  _ \/__ __\/  _ \/  __\
+| | //| ||  \/|  / \  | | ||| / \|| |     |  \/||  \   / \    |    \| || |\/|| | ||| |  | / \|  / \  | / \||  \/|
+| \// | ||    /  | |  | \_/|| |-||| |_/\  |  __/|  /_  | |    \___ || || |  || \_/|| |_/\ |-||  | |  | \_/||    /
+\__/  \_/\_/\_\  \_/  \____/\_/ \|\____/  \_/   \____\ \_/    \____/\_/\_/  \\____/\____|_/ \|  \_/  \____/\_/\_\                                                                                                                 
+");
+
+
+            WriteLine();
+
+            // variable declarations
+                     
+
+            
+
+            //create objects from virtualpets class
+            VirtualPet leoTheCat = new VirtualPet("Leo", 12, "Cat", true);
+            WriteLine("> PET 1:");
+            leoTheCat.Greet();
+            leoTheCat.Eat("fish");
+
+            WriteLine();
+
+            //create objects from virtualpets class
+            VirtualPet juniorTheParrot = new VirtualPet("Junior", 50, "Parrot", false);
+            WriteLine(">PET 2");
+            juniorTheParrot.Greet();
+            juniorTheParrot.Sleep();
+            juniorTheParrot.Eat("sunflower seeds");
+
+            WriteLine();
+
+            WriteLine("Press any key to exit...");
+            ReadKey();
         }
     }
 }
---
+++ VirtualPet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace VirtualPetSimulator
+{
+    class VirtualPet
+    {
+        public string? FullName;
+        public int Age;
+        public string? Species;
+        public double HungerLevel;
+        public bool IsAwake;
+        private int ExperiencePoints;
+
+        /// <summary>
+        /// constructor 
+        /// </summary>
+        public VirtualPet(string petName, 
+                          int petAge, 
+                          string petSpecies, 
+                          bool petIsAwake)
+        {
+            FullName = petName;
+            Age = petAge;
+            Species = petSpecies;
+            IsAwake = petIsAwake;
+        }
+
+        public void Greet()
+        {
+            
+            WriteLine($"My name is {FullName}, I am a {Species} and I am {Age} years young!");
+            WriteLine($"Is awake? {IsAwake}");
+        }
+
+        public void Sleep()
+        {
+            IsAwake = false;
+            WriteLine($"zZzZZzZZzzZ. {FullName} is now sleeping.");
+        }
+
+        public void Eat(string FoodName)
+        {
+            WriteLine($"{FullName} is now eating {FoodName}");
+        }
+
+    }
+}
